763fdb6a39b383ad2666dcbaa30d34f942c7dcbb
elisee <esabidani@gmail.com>
2022-04-09 01:38:22 +0000

    __Post_Artice__ OK

--- src/Article.js
+++ src/Article.js
@@ -2,13 +2,26 @@ import React from 'react';
 import "./styles/components/_article.scss"
 
 const Article = ({ article }) => {
-console.log(article);
+
+    const dataParser = (date) => {
+        let newDate = new Date(date).toLocaleDateString("fr-FR", {
+            year: "numeric",
+            month: 'long',
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric"
+        })
+        return newDate;
+    }
+
     return (
+
+
         <div className="article">
             <div className="card-header">
                 <div className="container">
                 <h3>{article.author}</h3>
-                <em>Posté le :{article.date}</em>
+                <em>Posté le :{dataParser(article.date)}</em>
                 </div>
                 <p>{article.content}</p>
 
--- src/News.jsx
+++ src/News.jsx
@@ -8,6 +8,8 @@ import "./_news.scss"
 const News = () => {
 
     const [newsData, setNewsData] = useState([])
+    const [author, setAuthor] = useState("")
+    const [content, setContent] = useState("")
 
     useEffect(() => {
         getData()
@@ -18,17 +20,34 @@ const News = () => {
         .get(" http://localhost:3001/articles")
         .then((res) => setNewsData(res.data))
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+        axios.post("http://localhost:3001/articles", {
+            author,
+            content,
+            date: Date.now()
+        }).then(() => {
+            setAuthor("");
+            setContent("");
+            getData()
+        })
+    }
+
     return (
 
         <div className="news-container">
             <h1>News</h1>
-            <form>
-                <input type="text" placeholder="Nom" />
-                <textarea placeholder="Message"></textarea>
+            <form onSubmit={(e) =>  handleSubmit(e)}>
+                <input onChange={(e) => setAuthor(e.target.value)} value={author} type="text" placeholder="Nom" />
+                <textarea onChange={(e) => setContent(e.target.value)} value={content} placeholder="Message"></textarea>
                 <input type="submit" value="Envoyez" />
             </form>
             {
-                newsData.map((article) => (
+                newsData
+                .sort((a,b) => b.data - a.data)
+                .map((article) => (
                     <Article key={article.id} article={article} />
                 ))
             }
